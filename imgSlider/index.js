e5e2febd7b28a9a06d82db5da03c5395c3b857e3
minji-2219 <aabb0219@naver.com>
2023-06-26 15:25:26 +0900

    slider complete

---
+++ imgSlider/index.js
@@ -0,0 +1,14 @@
+
+new Swiper('.swiper',{
+  loop:true,
+  centeredSlides: true,
+  pagination:{
+    el:'.swiper-pagination',
+    clickable:true
+  },
+  navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev'
+  },
+  
+})
